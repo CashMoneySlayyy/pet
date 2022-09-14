3ad4b9049552af4d706176b2d590228a8df28d6b
CashMoneySlayyy <113483097+CashMoneySlayyy@users.noreply.github.com>
2022-09-13 20:28:15 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showString("zzzzzzzzzzzzzz")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        # . # # .
+        # # # # #
+        # . # # .
+        . . . . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("Barbra Streisand")
 })
